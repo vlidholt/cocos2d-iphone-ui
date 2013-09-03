b287f89cc3c928dfec61d3837ac870e55ae4f469
Viktor <viktorlidholt@gmail.com>
2013-09-02 17:14:48 -0700

    Initial commit

---
+++ cocos2d-ui/cocos2d-ui/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  cocos2d-ui
+//
+//  Created by Viktor on 9/2/13.
+//  Copyright Apportable 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ cocos2d-ui/cocos2d-ui/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cocos2d-ui
+//
+//  Created by Viktor on 9/2/13.
+//  Copyright Apportable 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
